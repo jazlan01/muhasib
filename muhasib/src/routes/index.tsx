30e1f0c726b40ff202e9eab1a33ce31ae0813da1
Muhammad Jazlan <ja.zl.an@live.com>
2024-12-09 10:21:49 -0800

    Add new UI components and refactor routing structure

--- muhasib/src/routes/index.tsx
+++ muhasib/src/routes/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
@@ -7,8 +7,21 @@ export const Route = createFileRoute('/')({
 
 function HomeComponent() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+      <h1 className='text-6xl text-center font-semibold mb-16'>Welcome to <span className='font-bold text-primary'>Muhasib</span></h1>
+      <p className='text-xl max-w-96 text-center'>
+        Muhasib is a simple accounting app that helps you keep track of your
+        expenses and income.
+      </p>
+      <div className="mt-4 flex lg:flex-col gap-4">
+        <Button className='p-4 text-lg font-semibold'>
+          <a href="/login">Get Started</a>
+        </Button>
+        <Button className='p-4 text-lg font-semibold'>
+          <a href="/register">Register</a>
+        </Button>
+      </div>
+
     </div>
   )
 }
